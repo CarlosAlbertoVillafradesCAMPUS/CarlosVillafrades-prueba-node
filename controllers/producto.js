4eb79eda3e65a5c222639a230eb770819100f88e
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2024-03-07 11:50:16 -0500

    refactor: :recycle: refactor endpoint 1
    se realizo un refactor de la ocnuslta porque se habia dejado un where en la conuslta
    
    by:Carlos Villafrades

--- controllers/producto.js
+++ controllers/producto.js
@@ -6,9 +6,6 @@ export class productoController{
         try {
             const dataProductos = await productoModel.findAll(
                 {
-                    where:{
-                        id:1
-                    },
                     include:{
                         model: tiendaModel,
                         attributes:[["id", "idTienda"], "nombre", [sequelize.literal(`(SELECT SUM(cantidad) FROM productos_stocks WHERE id_producto = productos.id AND id_tienda = tiendas.id )`), 'stock'] ],
