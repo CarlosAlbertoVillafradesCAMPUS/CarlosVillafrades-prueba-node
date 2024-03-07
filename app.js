c757664f16111625906b9a548eaa906de6f180a9
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2024-03-07 10:26:13 -0500

    feat: :card_file_box: Create conexion a DB
    se cro la conexion a la base de datos
    
    by:Carlos Villafrades

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 import express from "express"
+import { envairoments } from "./config/envairoments.js"
+import { validateConexion } from "./db/connect.js";
+
+const appExpress = express();
+appExpress.use(express.json())
+appExpress.use(validateConexion)
+
+appExpress.get("/", (req,res)=>{
+    res.send("funcionando")
+})
+
+const port = envairoments.port;
+appExpress.listen(port, ()=>console.log(`servidor iniciado en el puerto: ${port}`))
